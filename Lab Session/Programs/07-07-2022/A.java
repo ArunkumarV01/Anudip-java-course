199b60f400f2f1074b118e891f68ea5fb3c33d13
ArunkumarV01 <108858991+ArunkumarV01@users.noreply.github.com>
2022-07-07 16:04:47 +0530

    Add files via upload

---
+++ Lab Session/Programs/07-07-2022/A.java
@@ -0,0 +1,17 @@
+class A
+{
+
+	public static void main(String []args)
+	{ int i=10;
+	  int j=11;
+	  int k=6;
+	    
+	    System.out.println("Increment of i " + i++);
+		System.out.println(i);
+		System.out.println(++i);
+		System.out.println("Decrement of j " + --j);
+		System.out.println(i+j + ++j);
+		System.out.println("value of k is "+k);
+        } 
+
+}
